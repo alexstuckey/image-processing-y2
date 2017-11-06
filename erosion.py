b149d0db60b486826cc66caac12ee7f04de6c3f7
Alex Stuckey <alex@stuckey.org.uk>
2017-11-06 14:47:06 +0000

    A bit of a work in progress

--- erosion.py
+++ erosion.py
@@ -9,6 +9,21 @@ filenameOut = sys.argv[2]
 img = cv2.imread(filenameIn, 0)
 
 struct = numpy.ones((5,5), numpy.uint8)
+origin = [3,3]
+print(struct.shape[0]-origin[0])
+img_eroded = numpy.zeros((img.shape[0],img.shape[1]), numpy.uint8)
+
+# For each pixel of input image
+for x in range(0, img.shape[1]):
+	for y in range(0, img.shape[0]):
+		# Fix the origin of the structuring element at x,y.
+		# Then traverse each pixel of struct and work out what's beneath it.
+		# And perform a reduce.
+		verticalMove   = y-origin[0]
+		horizontalMove = x-origin[1]
+		#verticalRange = range()
+		#print(vertical, horizontal)
+
 
 # A perfect erosion, performed by OpenCV's native method
 #img = cv2.erode(img, struct, iterations=1)
