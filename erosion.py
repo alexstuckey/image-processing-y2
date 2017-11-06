6504909f99db151c10041819b5cdf2e429efdfdb
Alex Stuckey <alex@stuckey.org.uk>
2017-11-06 14:13:41 +0000

    Imaging saving / displaying code too

--- erosion.py
+++ erosion.py
@@ -1,6 +1,17 @@
 import cv2
 import sys
 
-filename = sys.argv[1]
+filenameIn = sys.argv[1]
+filenameOut = sys.argv[2]
 
-print("filename is", filename)
+print("filename is", filenameIn)
+
+img = cv2.imread(filenameIn, 0)
+
+
+
+# Normally save image, but for dev we just show it
+#cv2.imwrite(filenameOut, img)
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
